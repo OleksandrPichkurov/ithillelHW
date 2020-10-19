5c00174d2b9b40c18d99023c474df364fdbd7388
RepkaPWNZ <alexandr.pichkurov@gmail.com>
2020-10-19 12:44:33 +0300

    changed init method in childs

--- OOP/kitchen.py
+++ OOP/kitchen.py
@@ -7,8 +7,8 @@ class Devices:
 
 class Stove(Devices):
 
-    def __init__(self, brand, color, material, burners, timer, price):
-        Devices.__init__(self, brand, color, material)
+    def __init__(self, burners, timer, price, *args):
+        super().__init__(*args)
         self.burners = burners
         self.timer = timer
         self.price = price
@@ -31,8 +31,8 @@ class Stove(Devices):
 
 class Teapot(Devices):
 
-    def __init__(self, brand, color, material):
-        Devices.__init__(self, brand, color, material)
+    def __init__(self, *args):
+        super().__init__(*args)
 
     def stay_warm(self, btn_on_off, initial_temp, set_temp):
         power = 'ON'
@@ -47,13 +47,13 @@ class Teapot(Devices):
 
         
 
-#s1 = Stove('LG', 'White', 'Steal', 4, True, 500)
-#s2 = Stove('Apple', 'Black', 'Steal', 6, False, 1000)
-#t1 = Teapot('Xiaomi', 'White', 'Plastic')
-#t2 = Teapot('Xiaomi', 'Black', 'Plastic')
-#print(s2.set_timer(1,1,1))
-#print(s1.color, s1.brand)
-#print(s1.turn_burner('ON'))
-#print(s2.material, s2.timer)
-#print(t1.color, t1.brand)
-#print(t2.stay_warm('off', 4, 3))
+s1 = Stove(4, True, 500, 'LG', 'White', 'Steal')
+s2 = Stove(6, False, 1000, 'Apple', 'Black', 'Steal')
+t1 = Teapot('Xiaomi', 'White', 'Plastic')
+t2 = Teapot('Xiaomi', 'Black', 'Plastic')
+print(s2.set_timer(1,1,1))
+print(s1.color, s1.brand)
+print(s1.turn_burner('ON'))
+print(s2.material, s2.timer)
+print(t1.material, t1.brand)
+print(t2.stay_warm('off', 4, 3))
